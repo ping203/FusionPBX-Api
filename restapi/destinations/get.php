abfac052583d5229c6296c052b0d74179d3ab437
aspurlock <aspurlock@iwsit.com>
2019-02-26 14:27:18 -0600

    Added type to destinations

--- restapi/destinations/get.php
+++ restapi/destinations/get.php
@@ -15,9 +15,16 @@ $message = array();
 
 $domain_uuid = (isset($_GET['domain_uuid']) ? $_GET['domain_uuid'] : null);
 
+/*
+ * Types include
+ * inbound
+ * outbound
+ * */
+$type = (isset($_GET['type']) ? $_GET['type'] : '');
+
 if ($domain_uuid = null) {
 
-    $sql = "select * from v_destinations ";
+    $sql = "select * from v_destinations where destination_type = '$type'";
     $prep_statement = $db->prepare(check_sql($sql));
     $prep_statement->execute();
     $destinations = $prep_statement->fetchAll(PDO::FETCH_NAMED);
@@ -25,7 +32,7 @@ if ($domain_uuid = null) {
 
 }else{
 
-    $sql = "select * from v_destinations where domain_uuid = '$domain_uuid'";
+    $sql = "select * from v_destinations where domain_uuid = '$domain_uuid' and destination_type = '$type'";
     $prep_statement = $db->prepare(check_sql($sql));
     $prep_statement->execute();
     $destinations = $prep_statement->fetchAll(PDO::FETCH_NAMED);
